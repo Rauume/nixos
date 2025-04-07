6c07c4be59935cffaa08514b0498cdefffaa8924
Cam Schriever <>
2025-04-07 22:33:06 +0930

    AMD gpu with LACT control.

--- hosts/modules/amdgpu.nix
+++ hosts/modules/amdgpu.nix
@@ -4,9 +4,21 @@
     enable32Bit = true;
   };
   
-  environment.systemPackages = with pkgs; [ lact ];
-  systemd.packages = with pkgs; [ lact ];
-  systemd.services.lactd.wantedBy = ["multi-user.target"];
+  #LACT gpu controller
+  environment.systemPackages = with pkgs; [
+    lact
+  ];
+
+  systemd.services.lact = {
+    description = "AMDGPU Control Daemon";
+    after = ["multi-user.target"];
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      ExecStart = "${pkgs.lact}/bin/lact daemon";
+    };
+    enable = true;
+  };
+
 
   systemd.tmpfiles.rules = [ "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}" ];
   services.xserver.videoDrivers = [ "amdgpu" ];
