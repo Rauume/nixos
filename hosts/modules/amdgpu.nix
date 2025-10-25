e5f061443555ea4e31fd2d78dd6b1093aaf2627e
Cam Schriever <>
2025-10-25 14:32:51 +1030

    refactor(amdgpu.nix): simplify amdgpu and lact configuration

--- hosts/modules/amdgpu.nix
+++ hosts/modules/amdgpu.nix
@@ -1,25 +1,4 @@
 {pkgs, ...}: {
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-  
-  #LACT gpu controller
-  environment.systemPackages = with pkgs; [
-    lact
-  ];
-
-  systemd.services.lact = {
-    description = "AMDGPU Control Daemon";
-    after = ["multi-user.target"];
-    wantedBy = ["multi-user.target"];
-    serviceConfig = {
-      ExecStart = "${pkgs.lact}/bin/lact daemon";
-    };
-    enable = true;
-  };
-
-
-  systemd.tmpfiles.rules = [ "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}" ];
-  services.xserver.videoDrivers = [ "amdgpu" ];
+  services.lact.enable = true;
+  hardware.amdgpu.overdrive.enable = true;
 }
